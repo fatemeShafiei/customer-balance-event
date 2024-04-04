30aeb21d83f003c0e07aef43923620d6ca0192d7
Shafiei <fateme.shafiei@capgemini.com>
2024-04-05 01:34:04 +0200

    feature integration test

--- routes/balanceEvents.js
+++ routes/balanceEvents.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const db = require("../models");
-const {validateBalanceEvent} = require('../models/balance_event.model');
+const {validateBalanceEvent} = require('../models/balanceEvent.model');
 const moment = require("moment");
 const {error} = require("winston");
+const Joi = require("joi");
 
 const BalanceEvents = db.balanceEvents;
 const Op = require('sequelize').Op;
@@ -11,7 +12,9 @@ const Op = require('sequelize').Op;
 
 router.post('/:market/:customerId', async (req, res) => {
     const {error} = validateBalanceEvent(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
 
     const randomId = (new Date()).getTime().toString(36) + Math.random().toString(36).slice(2);
     const balanceEvent = {
@@ -22,7 +25,7 @@ router.post('/:market/:customerId', async (req, res) => {
         reasonTime: moment(req.body.reasonTime).format("YYYY-MM-DD HH:mm:ss"),
         businessUnit: req.body.businessUnit,
         type: req.body.type,
-        value: req.body.type=== "DECREASED" ? -req.body.value : req.body.value,
+        value: req.body.type === "DECREASED" ? -req.body.value : req.body.value,
     };
 
     BalanceEvents.create(balanceEvent)
@@ -37,10 +40,16 @@ router.post('/:market/:customerId', async (req, res) => {
         });
 });
 
-router.get('/:market/:customerId/:reason/:reasonTime', async (req, res) => {
-    const year = Number(req.params.reasonTime);
+router.get('/:market/:customerId/:reason/:year', async (req, res) => {
+    const {error} = validateYear(req.params.year);
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
+    const year = Number(req.params.year);
+
     const yearTimeStamp = calculateTimeStamp(year);
     const nextYearTimeStamp = calculateTimeStamp(year + 1);
+
     const condition = {
         market: req.params.market,
         customerId: req.params.customerId,
@@ -59,6 +68,10 @@ router.get('/:market/:customerId/:reason/:reasonTime', async (req, res) => {
             const openingBalance = results[0] ? results[0] : 0;
             const closingBalance = results[0] + results[1];
             res.send({
+                customerId: req.params.customerId,
+                market: req.params.market,
+                activity: req.params.reason,
+                year: req.params.year,
                 openingBalance,
                 closingBalance
             })
@@ -86,6 +99,14 @@ async function sumValueOfBalances(condition) {
         }
     )
     return balance
+}
+
+function validateYear(reasonTime) {
+    const schema = Joi.object({
+        reasonTime: Joi.number().min(1970).max(3000).required(),
+    });
+
+    return schema.validate({reasonTime});
 }
 
 module.exports = router;
